b775a614706faebf99ef2ebe0d7c10611a642331
yudinaan <yudinaanastasiya01@mail.ru>
2023-05-25 00:58:12 +0500

    feat: added the opponent's move, corrected the victory check

--- kotlin_tic_tac_toe/src/main/kotlin/Main.kt
+++ kotlin_tic_tac_toe/src/main/kotlin/Main.kt
@@ -1,16 +1,23 @@
 fun main() {
     var field = Array(3, {Array(3, {"☐"})})
     output_field(field)
-    var round = 0
-    while (round < 9){
-        field = players_move(field)
-        field = opponents_move1(field)
 
-        if(round < 3) continue
+    players_move(field)
+    opponents_move1(field)
+
+    var round = 1
+    while (round <= 5){
+        round++
+        field = players_move(field)
 
         val and = victory_check(field)
-        if(and == 1) break
-        round++
+        println(and)
+        if(round == 5){
+            if(and == 0) println("Ничья!")
+        }
+        if(and == 1) continue
+
+        field = opponents_move(field)
     }
 
 }
@@ -31,16 +38,41 @@ fun players_move(field: Array<Array<String>>): Array<Array<String>> {
     n = readln().toInt() - 1
     print("Введите номер ячейки: ")
     c = readln().toInt() - 1
-    field[n][c] = "x"
+    field[n][c] = "X"
     output_field(field)
     return field
 }
 
-fun opponents_move1(field: Array<Array<String>>): Array<Array<String>> {
-    if(field[1][1] == "x"){
-        field[0][0] = "o"
+fun opponents_move(field: Array<Array<String>>): Array<Array<String>> {
+    var n = (0..2).random()
+    var m = (0..2).random()
+
+    val a = randomIndex(field,n,m)
+
+    if(a == true){
+        field[n][m] = "O"
+        println("Ход противника: ")
+        output_field(field)
     }
-    else field[1][1] = "o"
+    else opponents_move(field)
+
+    return field
+}
+fun randomIndex(field: Array<Array<String>>, n: Int, m: Int): Boolean {
+
+    val a = if(field[n][m] == "☐"){
+        return true
+    }
+    else return false
+
+    return a
+}
+
+fun opponents_move1(field: Array<Array<String>>): Array<Array<String>> {
+
+    if (field[1][1] == "X") {
+        field[0][0] = "O"
+    } else field[1][1] = "O"
 
     println("Ход противника: ")
     output_field(field)
@@ -50,146 +82,244 @@ fun opponents_move1(field: Array<Array<String>>): Array<Array<String>> {
 
 
 fun victory_check(field: Array<Array<String>>): Int {
-    val arrayx = Array(3, { Array(3, { "x" }) })
-    val arrayo = Array(3, { Array(3, { "o" }) })
+    val arrayx = Array(3, { Array(3, { "X" }) })
+    val arrayo = Array(3, { Array(3, { "O" }) })
 
     var and = 0
 
     var a = 0
-    while (a < 3) {
-        and = if (field[a] == arrayx[a]) {
-            println("Вы победили!")
-             return 1
-        }
-        else if (field[a] == arrayo[a]) {
-            println("Победил противник!")
-            return 1
-        }
-        else  return 0
-        a++
-
-    }
-
-    a = 0
     var b = 0
-     if (field[a][b] == arrayx[a][b]) {
+    if (field[a][b] == arrayx[a][b]) {
         b++
         if (field[a][b] == arrayx[a][b]) {
             b++
-            and = if (field[a][b] == arrayx[a][b]) {
+            if (field[a][b] == arrayx[a][b]) {
                 println("Вы победили!")
-                return 1
+                and = 1
             }
-            else return 0
+            else and = 0
         }
     }
 
-
-    a = 0
+    a = 1
     b = 0
     if (field[a][b] == arrayx[a][b]) {
-        a++
-        if (field[a][b] == arrayx[a][b]) {
-            a++
-            and = if (field[a][b] == arrayx[a][b]) {
-                println("Вы победили!")
-                return 1
-            }
-            else return 0
-        }
-    }
-
-    a = 0
-    b = 0
-    if (field[a][b] == arrayo[a][b]) {
-        b++
-        if (field[a][b] == arrayo[a][b]) {
-            b++
-            and =if (field[a][b] == arrayo[a][b]) {
-                println("Победил противник!")
-                return 1
-            }
-            else return 0
-        }
-    }
-
-    a = 0
-    b = 0
-    if (field[a][b] == arrayo[a][b]) {
-        a++
-        if (field[a][b] == arrayo[a][b]) {
-            a++
-            and = if (field[a][b] == arrayo[a][b]) {
-                println("Победил противник!")
-                return 1
-            }
-            else return 0
-        }
-    }
-
-    a = 0
-    b = 0
-    if (field[a][b] == arrayx[a][b]) {
-        a++
         b++
         if (field[a][b] == arrayx[a][b]) {
-            a++
             b++
-            and = if (field[a][b] == arrayx[a][b]) {
+            if (field[a][b] == arrayx[a][b]) {
                 println("Вы победили!")
-                return 1
+                and = 1
             }
-            else return 0
-        }
-    }
-
-    a = 0
-    b = 0
-    if (field[a][b] == arrayo[a][b]) {
-        a++
-        b++
-        if (field[a][b] == arrayo[a][b]) {
-            a++
-            b++
-            and = if (field[a][b] == arrayo[a][b]) {
-                println("Победил противник!")
-                return 1
-            }
-            else return 0
+            else and = 0
         }
     }
 
     a = 2
-    b = 2
+    b = 0
     if (field[a][b] == arrayx[a][b]) {
-        a--
-        b--
+        b++
         if (field[a][b] == arrayx[a][b]) {
-            a--
-            b--
-            and = if (field[a][b] == arrayx[a][b]) {
+            b++
+            if (field[a][b] == arrayx[a][b]) {
                 println("Вы победили!")
-                return  1
+                and = 1
             }
-            else return 0
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 0
+    if (field[a][b] == arrayo[a][b]) {
+        b++
+        if (field[a][b] == arrayo[a][b]) {
+            b++
+            if (field[a][b] == arrayo[a][b]) {
+                println("Победил противник!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 1
+    b = 0
+    if (field[a][b] == arrayo[a][b]) {
+        b++
+        if (field[a][b] == arrayo[a][b]) {
+            b++
+            if (field[a][b] == arrayo[a][b]) {
+                println("Победил противник!")
+                and = 1
+            }
+            else and = 0
         }
     }
 
     a = 2
+    b = 0
+    if (field[a][b] == arrayo[a][b]) {
+        b++
+        if (field[a][b] == arrayo[a][b]) {
+            b++
+            if (field[a][b] == arrayo[a][b]) {
+                println("Победил противник!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+
+    a = 0
+    b = 0
+    if (field[a][b] == arrayx[a][b]) {
+        a++
+        if (field[a][b] == arrayx[a][b]) {
+            a++
+            if (field[a][b] == arrayx[a][b]) {
+                println("Вы победили!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 1
+    if (field[a][b] == arrayx[a][b]) {
+        a++
+        if (field[a][b] == arrayx[a][b]) {
+            a++
+             if (field[a][b] == arrayx[a][b]) {
+                 println("Вы победили!")
+                 and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 2
+    if (field[a][b] == arrayx[a][b]) {
+        a++
+        if (field[a][b] == arrayx[a][b]) {
+            a++
+            if (field[a][b] == arrayx[a][b]) {
+                println("Вы победили!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 0
+    if (field[a][b] == arrayo[a][b]) {
+        a++
+        if (field[a][b] == arrayo[a][b]) {
+            a++
+            if (field[a][b] == arrayo[a][b]) {
+                println("Победил противник!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 1
+    if (field[a][b] == arrayo[a][b]) {
+        a++
+        if (field[a][b] == arrayo[a][b]) {
+            a++
+            if (field[a][b] == arrayo[a][b]) {
+                println("Победил противник!")
+                and = 1
+            }
+            else and = 1
+        }
+    }
+
+    a = 0
     b = 2
     if (field[a][b] == arrayo[a][b]) {
-        a--
+        a++
+        if (field[a][b] == arrayo[a][b]) {
+            a++
+            if (field[a][b] == arrayo[a][b]) {
+                println("Победил противник!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 0
+    if (field[a][b] == arrayx[a][b]) {
+        a++
+        b++
+        if (field[a][b] == arrayx[a][b]) {
+            a++
+            b++
+            if (field[a][b] == arrayx[a][b]) {
+                println("Вы победили!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 0
+    if (field[a][b] == arrayo[a][b]) {
+        a++
+        b++
+        if (field[a][b] == arrayo[a][b]) {
+            a++
+            b++
+            if (field[a][b] == arrayo[a][b]) {
+                println("Победил противник!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 2
+    if (field[a][b] == arrayx[a][b]) {
+        a++
+        b--
+        if (field[a][b] == arrayx[a][b]) {
+            a++
+            b--
+            if (field[a][b] == arrayx[a][b]) {
+                println("Вы победили!")
+                and = 1
+            }
+            else and = 0
+        }
+    }
+
+    a = 0
+    b = 2
+    if (field[a][b] == arrayo[a][b]) {
+        a++
         b--
         if (field[a][b] == arrayo[a][b]) {
-            a--
+            a++
             b--
-            and = if (field[a][b] == arrayo[a][b]) {
+            if (field[a][b] == arrayo[a][b]) {
                 println("Победил противник!")
-                return 1
+                and = 1
             }
-            else return 0
+            else and = 1
         }
     }
 
     return and
 }
+
